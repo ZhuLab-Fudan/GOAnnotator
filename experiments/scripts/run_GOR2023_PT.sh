d1510d70b827a2abcc5faaff98228de8245771a5
Huiying Yan <hyyan23@m.fudan.edu.cn>
2025-02-14 19:29:28 +0800

    update scripts

--- experiments/scripts/run_GOR2023_PT.sh
+++ experiments/scripts/run_GOR2023_PT.sh
@@ -13,10 +13,11 @@ for i in ${!tasks[@]}; do
     MAP="../results/GOR2023/map/${task}_PT.map"
     INPUT="../../data/GOR2023/pid/pubtator.pid"
     log_file="../logs/${task}_pub.log"
-
+    (
     CUDA_VISIBLE_DEVICES=$gpu python ../../src/run_pubretriever.py --input $INPUT --output $MAP --task $task > $log_file 2>&1 
 
     SAVE_DIR="../results/GOR2023/"
     log_file="../logs/${task}_gor.log"
-    CUDA_VISIBLE_DEVICES=$gpu python ../../src/run_goretrieverplus.py --save_dir $SAVE_DIR --task $task --data $DATA --input $MAP --pro_num $pro --filter_rank True >> $log_file 2>&1 &
+    CUDA_VISIBLE_DEVICES=$gpu python ../../src/run_goretrieverplus.py --save_dir $SAVE_DIR --task $task --data $DATA --input $MAP --pro_num $pro --filter_rank True >> $log_file 2>&1 
+    ) &
 done
--- experiments/scripts/run_TR2024_SP.sh
+++ experiments/scripts/run_TR2024_SP.sh
@@ -13,10 +13,11 @@ for i in ${!tasks[@]}; do
     MAP="../results/TR2024/map/${task}_SP.map"
     INPUT="../../data/TR2024/pid/${task}_SP.pid"
     log_file="../logs/${task}_pub.log"
-
+    (
     CUDA_VISIBLE_DEVICES=$gpu python ../../src/run_pubretriever.py --input $INPUT --output $MAP --task $task > $log_file 2>&1 
 
     SAVE_DIR="../results/TR2024/"
     log_file="../logs/${task}_gor.log"
-    CUDA_VISIBLE_DEVICES=$gpu python ../../src/run_goretrieverplus.py --save_dir $SAVE_DIR --task $task --data $DATA --input $MAP --pro_num $pro --filter_rank True >> $log_file 2>&1 &
+    CUDA_VISIBLE_DEVICES=$gpu python ../../src/run_goretrieverplus.py --save_dir $SAVE_DIR --task $task --data $DATA --input $MAP --pro_num $pro --filter_rank True >> $log_file 2>&1 
+    ) &
 done
