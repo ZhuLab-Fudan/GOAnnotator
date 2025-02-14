ab62ee8e6e730dadb9917b234d1d19bb428d37e0
Huiying Yan <hyyan23@m.fudan.edu.cn>
2025-02-14 19:34:34 +0800

    update scripts

--- experiments/scripts/run.sh
+++ experiments/scripts/run.sh
@@ -13,10 +13,11 @@ for i in ${!tasks[@]}; do
     MAP="../results/${DATA}/map/${task}.map"
     INPUT="../../data/${DATA}/pid/${task}.pid"
     log_file="../logs/${task}_pub.log"
-
+    (
     CUDA_VISIBLE_DEVICES=$gpu python ../../src/run_pubretriever.py --input $INPUT --output $MAP --task $task > $log_file 2>&1 
 
     SAVE_DIR="results/${DATA}/"
     log_file="../logs/${task}_gor.log"
-    CUDA_VISIBLE_DEVICES=$gpu python ../../src/run_goretrieverplus.py --save_dir $SAVE_DIR --task $task --data $DATA --input $MAP --pro_num $pro --filter_rank True >> $log_file 2>&1 &
+    CUDA_VISIBLE_DEVICES=$gpu python ../../src/run_goretrieverplus.py --save_dir $SAVE_DIR --task $task --data $DATA --input $MAP --pro_num $pro --filter_rank True >> $log_file 2>&1
+    ) &
 done
