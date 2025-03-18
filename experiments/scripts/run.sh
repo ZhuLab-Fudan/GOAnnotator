0f734ac4f9ac33dd9ddf737392f2bd340a969aa8
Huiying Yan <hyyan23@m.fudan.edu.cn>
2025-03-18 20:50:17 +0800

    Updata

--- experiments/scripts/run.sh
+++ experiments/scripts/run.sh
@@ -11,13 +11,14 @@ for i in ${!tasks[@]}; do
     pro=${pros[$i]}
     gpu=${gpus[$i]}
     MAP="../results/${DATA}/map/${task}.map"
-    INPUT="../../data/${DATA}/pid/${task}.pid"
-    log_file="../logs/${task}_pub.log"
-    (
-    CUDA_VISIBLE_DEVICES=$gpu python ../../src/run_pubretriever.py --input $INPUT --output $MAP --task $task > $log_file 2>&1 
+    INPUT="../../data/${DATA}/${task}.pid"
+    SAVE_DIR="../results/${DATA}/"
 
-    SAVE_DIR="results/${DATA}/"
-    log_file="../logs/${task}_gor.log"
-    CUDA_VISIBLE_DEVICES=$gpu python ../../src/run_goretrieverplus.py --save_dir $SAVE_DIR --task $task --data $DATA --input $MAP --pro_num $pro --filter_rank True >> $log_file 2>&1
+    (
+        log_file="../logs/${task}_pub.log"
+        CUDA_VISIBLE_DEVICES=$gpu python ../../src/run_pubretriever.py --input $INPUT --output $MAP --task $task > $log_file 2>&1 
+
+        log_file="../logs/${task}_gor.log"
+        CUDA_VISIBLE_DEVICES=$gpu python ../../src/run_goretrieverplus.py --save_dir $SAVE_DIR --task $task --data $DATA --input $MAP --pro_num $pro --filter_rank True > $log_file 2>&1 
     ) &
 done
