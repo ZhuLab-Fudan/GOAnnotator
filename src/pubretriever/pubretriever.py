5242987b78cc13a983f7d0379ed83bf5dcca98fd
Huiying Yan <hyyan23@m.fudan.edu.cn>
2026-07-02 19:35:27 +0800

    fixed meta_FGR

--- src/pubretriever/pubretriever.py
+++ src/pubretriever/pubretriever.py
@@ -1,6 +1,7 @@
 import os
 import json
 from typing import List, Dict
+
 from sentence_transformers import CrossEncoder
 from pyserini.search.lucene import LuceneSearcher
 from tqdm import tqdm
@@ -8,33 +9,52 @@ import numpy as np
 
 # Import utilities
 from pubretriever.utils import average_scores
-from utils import TASK_DEFINITIONS, filter_text, filter_species, extract_for_FIR, extract_for_train, Config
+from utils import (
+    TASK_DEFINITIONS,
+    filter_text,
+    filter_species,
+    extract_for_FIR,
+    extract_for_train,
+    Config,
+)
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 
 class PubRetriever:
     """
     A class for protein-related literature retrieval and reranking for function annotation.
     """
 
-    def __init__(self, config: Config, model_path: str, input: str, output: str, k: int = 50, task: str = "mf"):
+    def __init__(
+        self,
+        config: Config,
+        model_path: str,
+        input: str,
+        output: str,
+        k: int = 50,
+        task: str = "mf",
+    ):
         """
         Initialize the PubRetriever.
-        
+
         Args:
+            config (Config): Configuration object.
             model_path (str): Path to the pre-trained CrossEncoder model.
-            input (List[str]): List of protein IDs to process.
+            input (str): Path to input protein ID file.
+            output (str): Path to output result file.
             k (int): Number of documents to retrieve. Default is 50.
             task (str): Annotation task type ('mf', 'bp', 'cc'). Default is 'mf'.
         """
         self.model = CrossEncoder(model_path, max_length=512)
         self.searcher = LuceneSearcher(config.PMID_INDEX_PATH)
         self.metadata = np.load(config.METADATA_PATH, allow_pickle=True).item()
-        self.input = input  
+
+        self.input = input
         self.output = output
         self.task = task
         self.k = k
-        
+
     def full_information_retrieval(self, protein_metadata: Dict) -> List[Dict[str, str]]:
         """
         Perform full information retrieval (FIR) for a given protein.
@@ -45,20 +65,29 @@ class PubRetriever:
         Returns:
             List[Dict[str, str]]: A list of retrieved documents with PMID and text content.
         """
-        protein_name = extract_for_FIR(protein_metadata).replace('gene', '').replace('protein', '')
+        protein_name = extract_for_FIR(protein_metadata).replace("gene", "").replace("protein", "")
         query = f"function or role of {protein_name}"
+
         results = self.searcher.search(query, self.k)
+
         pmids = []
         for result in results:
             pmid = json.loads(result.raw)
-            pmids.append({
-                'pmid': pmid['id'],
-                'text': pmid['contents']
-            })
-        return pmids
-        
+            pmids.append(
+                {
+                    "pmid": pmid["id"],
+                    "text": pmid["contents"],
+                }
+            )
 
-    def meta_FGR(self, query: str, weight: float) -> Dict[str, List[float]]:
+        return pmids
+
+    def meta_FGR(
+        self,
+        query: str,
+        scores: Dict[str, List[float]],
+        weight: float,
+    ) -> Dict[str, List[float]]:
         """
         Perform fine-grained retrieval (FGR) for a given query and update scores.
         
@@ -69,13 +98,18 @@ class PubRetriever:
         Returns:
             Dict[str, List[float]]: A dictionary of document IDs and their scores.
         """
-        scores = {}
+        if len(query) < 3:
+            print("Short query: ", query)
+            return scores
+
         results = self.searcher.search(query, self.k)
+
         for result in results:
-            pmid = json.loads(result.raw)['id']
+            pmid = json.loads(result.raw)["id"]
             scores.setdefault(pmid, []).append(weight * result.score)
+
         return scores
-    
+
     def fine_grained_retrieval(self, protein_metadata: Dict) -> List[Dict[str, str]]:
         """
         Perform fine-grained retrieval (FGR) for a given protein using its metadata.
@@ -86,71 +120,130 @@ class PubRetriever:
         Returns:
             List[Dict[str, str]]: A list of retrieved documents with PMID and text content.
         """
-        scores = {}
+        scores: Dict[str, List[float]] = {}
+
+        if not protein_metadata:
+            return []
+
         names = protein_metadata.get("gene name", {}).get("name", [])
+
         if len(names) > 0:
             for item in names:
-                if item.get('Name'):
-                    query = filter_text(item['Name'])
-                    scores = self.meta_FGR(query, 1/len(query.split(' ')))
+                if item.get("Name"):
+                    query = filter_text(item["Name"])
+                    scores = self.meta_FGR(
+                        query,
+                        scores,
+                        1 / len(query.split(" ")),
+                    )
+
                     if len(scores) >= self.k:
-                        query = (filter_text(item['Name']) + ' ') * 4 + ' ' + filter_species(protein_metadata['species'])
-                        scores = self.meta_FGR(query, 1/len(query.split(' ')))
-                elif item.get('Synonyms') and item['Synonyms'] != 'N/A':
-                    query = filter_text(item['Synonyms'])
-                    scores = self.meta_FGR(query, 1/len(query.split(' ')))
+                        query = (
+                            (filter_text(item["Name"]) + " ") * 4
+                            + " "
+                            + filter_species(protein_metadata["species"])
+                        )
+                        scores = self.meta_FGR(
+                            query,
+                            scores,
+                            1 / len(query.split(" ")),
+                        )
+
+                elif item.get("Synonyms") and item["Synonyms"] != "N/A":
+                    query = filter_text(item["Synonyms"])
+                    scores = self.meta_FGR(
+                        query,
+                        scores,
+                        1 / len(query.split(" ")),
+                    )
+
                     if len(scores) >= self.k:
-                        query = (filter_text(item['Synonyms']) + ' ') * 4 + ' ' + filter_species(protein_metadata['species'])
-                        scores = self.meta_FGR(query, 1/len(query.split(' ')))
-        
+                        query = (
+                            (filter_text(item["Synonyms"]) + " ") * 4
+                            + " "
+                            + filter_species(protein_metadata["species"])
+                        )
+                        scores = self.meta_FGR(
+                            query,
+                            scores,
+                            1 / len(query.split(" ")),
+                        )
+
         if protein_metadata.get("short name"):
             if isinstance(protein_metadata["short name"], str):
                 query = filter_text(protein_metadata["short name"])
-                scores = self.meta_FGR(query, 1/len(query.split(' ')))
+                scores = self.meta_FGR(
+                    query,
+                    scores,
+                    1 / len(query.split(" ")),
+                )
+
             elif isinstance(protein_metadata["short name"], list):
                 for item in protein_metadata["short name"]:
                     query = filter_text(item)
-                    scores = self.meta_FGR(query, 1/len(query.split(' ')))  
-                    
+                    scores = self.meta_FGR(
+                        query,
+                        scores,
+                        1 / len(query.split(" ")),
+                    )
+
         if len(scores) >= self.k:
             for pmid in scores:
                 scores[pmid] = average_scores(scores[pmid])
-            pmids = sorted(scores, key=lambda x: scores[x], reverse=True)[:self.k]
+
+            pmids = sorted(scores, key=lambda x: scores[x], reverse=True)[: self.k]
+
             res = []
             for pmid in pmids:
-                pmid = json.loads(self.searcher.doc(pmid).raw())
-                res.append({
-                    'pmid': pmid['id'],
-                    'text': pmid['contents']
-                })
+                doc = json.loads(self.searcher.doc(pmid).raw())
+                res.append(
+                    {
+                        "pmid": doc["id"],
+                        "text": doc["contents"],
+                    }
+                )
+
             return res
-        
-        if protein_metadata.get('full name'):
-            if protein_metadata['full name'].get('RecName'):
-                query = filter_text(protein_metadata['full name']['RecName']) 
-                scores = self.meta_FGR(query, 1/len(query.split(' ')))
-            if protein_metadata['full name'].get('AltName'):
-                for item in protein_metadata['full name']['AltName'].split(','):
-                    query = filter_text(item) 
-                    scores = self.meta_FGR(query, 1/len(query.split(' ')))
-        
+
+        if protein_metadata.get("full name"):
+            if protein_metadata["full name"].get("RecName"):
+                query = filter_text(protein_metadata["full name"]["RecName"])
+                scores = self.meta_FGR(
+                    query,
+                    scores,
+                    1 / len(query.split(" ")),
+                )
+
+            if protein_metadata["full name"].get("AltName"):
+                for item in protein_metadata["full name"]["AltName"].split(","):
+                    query = filter_text(item)
+                    scores = self.meta_FGR(
+                        query,
+                        scores,
+                        1 / len(query.split(" ")),
+                    )
+
         for pmid in scores:
             try:
                 scores[pmid] = average_scores(scores[pmid])
             except TypeError:
                 print(scores, protein_metadata)
-        
-        pmids = sorted(scores, key=lambda x: scores[x], reverse=True)[:self.k]
+
+        pmids = sorted(scores, key=lambda x: scores[x], reverse=True)[: self.k]
+
         res = []
         for pmid in pmids:
-            pmid = json.loads(self.searcher.doc(pmid).raw())
-            res.append({
-                'pmid': pmid['id'],
-                'text': pmid['contents']
-            })
+            doc = json.loads(self.searcher.doc(pmid).raw())
+            res.append(
+                {
+                    "pmid": doc["id"],
+                    "text": doc["contents"],
+                }
+            )
+
         return res
 
-    def hybrid_retrieval(self, protein_metadata: Dict) -> List[Dict[str, str]]:
+    def hybrid_retrieval(self, proid: str, protein_metadata: Dict) -> List[Dict[str, str]]:
         """
         Perform hybrid retrieval by combining results from FIR and FGR.
         
@@ -162,11 +255,15 @@ class PubRetriever:
         """
         FIR_results = self.full_information_retrieval(protein_metadata)
         FGR_results = self.fine_grained_retrieval(protein_metadata)
-        merged_dict = {item['pmid']: item for item in FIR_results + FGR_results}
-        return list(merged_dict.values())
-        
 
-    def rerank_documents(self, protein_metadata: Dict, documents: List[Dict[str, str]]) -> List[Dict[str, str]]:
+        merged_dict = {item["pmid"]: item for item in FIR_results + FGR_results}
+        return list(merged_dict.values())
+
+    def rerank_documents(
+        self,
+        protein_metadata: Dict,
+        documents: List[Dict[str, str]],
+    ) -> List[Dict[str, str]]:
         """
         Rerank retrieved documents using the CrossEncoder model.
         
@@ -178,14 +275,28 @@ class PubRetriever:
             List[Dict[str, str]]: A sorted list of documents with reranked scores.
         """
         protein_name = extract_for_train(protein_metadata)
+
+        print("====run task:", self.task)
+
         query = f"{protein_name} The {TASK_DEFINITIONS[self.task]} is "
-        inputs = [[query, doc['text']] for doc in documents]
-        scores = self.model.predict(inputs, batch_size=96, show_progress_bar=False)
+        inputs = [[query, doc["text"]] for doc in documents]
+
+        scores = self.model.predict(
+            inputs,
+            batch_size=96,
+            show_progress_bar=False,
+        )
+
         for i, doc in enumerate(documents):
-            doc['score'] = scores[i]
-        return sorted(documents, key=lambda x: x['score'], reverse=True)[:10]
-    
-    def retrieve_and_rerank(self, protein_metadata: Dict) -> List[Dict[str, str]]:
+            doc["score"] = scores[i]
+
+        return sorted(documents, key=lambda x: x["score"], reverse=True)[:10]
+
+    def retrieve_and_rerank(
+        self,
+        proid: str,
+        protein_metadata: Dict,
+    ) -> List[Dict[str, str]]:
         """
         Retrieve and rerank documents for a given protein.
         
@@ -195,7 +306,12 @@ class PubRetriever:
         Returns:
             List[Dict[str, str]]: Top reranked documents.
         """
-        hybrid_retrieval_results = self.hybrid_retrieval(protein_metadata)
+        hybrid_retrieval_results = self.hybrid_retrieval(proid, protein_metadata)
+
+        if len(hybrid_retrieval_results) < 1:
+            print(proid)
+            return []
+
         return self.rerank_documents(protein_metadata, hybrid_retrieval_results)
 
     def process_proteins(self):
@@ -207,12 +323,21 @@ class PubRetriever:
         """
         with open(self.input) as input_file:
             with open(self.output, "w") as output_file:
-                proids = [_.strip() for _ in input_file.readlines()]
+                proids = [line.strip() for line in input_file.readlines()]
+
                 for proid in tqdm(proids, desc="Processing proteins"):
                     if proid not in self.metadata:
                         print(f"Metadata missing for Protein ID: {proid}")
                         continue
+
                     protein_metadata = self.metadata.get(proid, {})
-                    documents = self.retrieve_and_rerank(protein_metadata)
+                    documents = self.retrieve_and_rerank(proid, protein_metadata)
+
+                    if len(documents) < 1:
+                        print(proid)
+                        continue
+
                     for doc in documents:
-                        output_file.write(f"{proid}\t{doc['pmid']}\t{doc['score']:.4f}\n")
+                        output_file.write(
+                            f"{proid}\t{doc['pmid']}\t{doc['score']}\n"
+                        )
